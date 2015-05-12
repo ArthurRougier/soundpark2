5ba867961ad48f7cfe05ab3e63f869448ec72370
ArthurRougier <arthur.rougier@gmail.com>
2015-05-12 15:31:22 +0200

    Radio Mode test

--- model/get_dislike_state.php
+++ model/get_dislike_state.php
@@ -1,6 +1,6 @@
 <?php
 	include_once('connect_sql.php');
-	$req = $bdd->prepare('SELECT dislike.ID FROM soundpark2.dislike, user, song WHERE dislike.ID_song = song.ID AND song.trackId = ? AND dislike.ID_user = user.ID AND user.email=?');
+	$req = $bdd->prepare('SELECT dislike.ID FROM soundpark2.dislike, user, song WHERE dislike.ID_song = song.ID AND song.trackId = ? AND dislike.ID_user = user.ID AND user.ID=?');
 	$req->execute(array(
 		$_GET['trackId'],
 		$_GET['currentUser']
