226edf06b324e02113c36899aa31f2af772ab577
ArthurRougier <arthur.rougier@gmail.com>
2015-08-07 18:19:25 +0200

    Playlist tiles update

--- control/display_settings_playlist_tiles.php
+++ control/display_settings_playlist_tiles.php
@@ -9,9 +9,15 @@
 		?>
 		<div id="box<?php echo($i);?>" class="box"
 			><div class="boxInner" id="boxInner<?php echo($i);?>"
-				><div class="playlist_overlay <?php 
+				><figure class="playlist_overlay stack-randomrot">
+					<img src="<?php echo($playlistBoxes['pic1']) ?>" alt="img01"/>
+					<img src="<?php echo($playlistBoxes['pic2']) ?>" alt="img02"/>
+					<img src="<?php echo($playlistBoxes['pic3']) ?>" alt="img03"/>
+				</figure>
 
-					switch ($j) {
+				<!-- <div class="playlist_overlay <?php 
+
+					/*switch ($j) {
 					    case 1:
 					        echo "purple";
 					        break;
@@ -28,10 +34,11 @@
 			                echo "black";
 			                $j = 0;
 			                break;
-					}
+					}*/
 
 				 ?>" id="playlist_overlay<?php echo($i);?>"
-					><a href="../view/frommail.php?playlistId=<?php echo($playlistBoxes[0]); ?>" target="_blank"><h2><?php
+					> -->
+					<a href="../view/frommail.php?playlistId=<?php echo($playlistBoxes[0]); ?>" target="_blank"><h2><?php
 						$ddate = $playlistBoxes[1];
 						$date = new DateTime($ddate);
 						$week = $date->format("W");
@@ -45,15 +52,36 @@
 
 
 
-						echo $monthLetter.", week #".$adjustedWeek;
+						echo $monthLetter.", playlist #".$playlistBoxes[0];
 
 
 				 	?></h2><div class="helper"></div></a
 				></div
-			></div
-		></div>
+			></div>
 		<?php 
 		$i++;
 		$j++;
 	};
 
+
+?>
+
+<script type="text/javascript">
+	var allTiles = document.querySelectorAll('.box');
+	for(var indexTilesPlaylist = 0 ; indexTilesPlaylist < allTiles.length ; indexTilesPlaylist++ )
+	{
+		allTiles[indexTilesPlaylist].addEventListener('mouseover', function(){
+			this.firstChild.firstChild.classList.add("active");
+		}, false);
+	}
+	for(var indexTilesPlaylist = 0 ; indexTilesPlaylist < allTiles.length ; indexTilesPlaylist++ )
+	{
+		allTiles[indexTilesPlaylist].addEventListener('mouseout', function(){
+			this.firstChild.firstChild.classList.remove("active");
+		}, false);
+	}
+	/*addEventListener('click', function(){
+			this.classList.add("active");
+	}, false);*/
+
+</script>
--- model/get_settings_playlist_tiles.php
+++ model/get_settings_playlist_tiles.php
@@ -2,7 +2,13 @@
 	
 	if(isset($_COOKIE['current_user']))
 	{
-		$req = $bdd->query('SELECT ID, date_start, date_end FROM playlist WHERE date_end <= NOW() ORDER BY ID DESC');
+		$req = $bdd->query('SELECT ID, date_start, date_end,
+				(SELECT artwork_url FROM song WHERE song.ID_playlist = a.ID LIMIT 1) as pic1,
+				(SELECT artwork_url FROM song WHERE song.ID_playlist = a.ID LIMIT 1, 1) as pic2,
+				(SELECT artwork_url FROM song WHERE song.ID_playlist = a.ID LIMIT 2, 1) as pic3,
+				(SELECT artwork_url FROM song WHERE song.ID_playlist = a.ID LIMIT 3, 1) as pic4
+			FROM playlist a WHERE date_end <= NOW() ORDER BY ID DESC'
+		);
 	}
 
 
