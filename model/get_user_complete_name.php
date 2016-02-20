66945b148e1dcb48da0b549adaff8f311bb898ef
ArthurRougier <arthur.rougier@gmail.com>
2016-02-20 19:18:47 +0100

    Display curator pseudo if any

--- model/get_user_complete_name.php
+++ model/get_user_complete_name.php
@@ -3,29 +3,43 @@
 	include_once('connect_sql.php');
 	if(isset($_COOKIE['current_user']))
 	{
-		$req = $bdd->prepare('SELECT first_name, last_name FROM facebook_account WHERE ID_user = ?');
+		$req = $bdd->prepare('SELECT pseudo FROM curator WHERE ID_user = ?');
 		$req->execute(array($_COOKIE['current_user']));
 		if($names = $req->fetch())
 		{
-			$completeName = $names[0] . ' ' . $names[1];
+			$completeName = $names[0];
 			if(isset($_GET['displayResult']))
 			{
 				echo $completeName;
 			}
 		}
+
 		else
 		{
-			$req = $bdd->prepare('SELECT email FROM user WHERE ID = ?');
+			$req = $bdd->prepare('SELECT first_name, last_name FROM facebook_account WHERE ID_user = ?');
 			$req->execute(array($_COOKIE['current_user']));
 			if($names = $req->fetch())
 			{
-				$completeName = $names[0];
+				$completeName = $names[0] . ' ' . $names[1];
 				if(isset($_GET['displayResult']))
 				{
 					echo $completeName;
 				}
 			}
-		}
+			else
+			{
+				$req = $bdd->prepare('SELECT email FROM user WHERE ID = ?');
+				$req->execute(array($_COOKIE['current_user']));
+				if($names = $req->fetch())
+				{
+					$completeName = $names[0];
+					if(isset($_GET['displayResult']))
+					{
+						echo $completeName;
+					}
+				}
+			}
+		}		
 	}
 	else
 	{
